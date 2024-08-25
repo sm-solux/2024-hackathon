538941e13df3241aa374587b2c8ce6cbf5e9f7ac
김세희 <lolry0207@sookmyung.ac.kr>
2024-08-25 19:18:14 +0900

    mod: topbar pathname 수정

--- src/component/Topbar.jsx
+++ src/component/Topbar.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useLocation, Link } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-// import Button from 'react-bootstrap/Button';
 import Button from "./Button";
 import '../style/Topbar.css';
-import topbarlogo from "../images/logo/logo_white.png"
+import topbarlogo from "../images/logo/logo_white.png";
 
 function Topbar() {
   const location = useLocation();
   const isMargin = useMediaQuery({ query: "(min-width: 995px)" });
+  
+  const [activeLink, setActiveLink] = useState('/');
+
+  useEffect(() => {
+    setActiveLink(location.pathname);
+  }, [location.pathname]);
 
   return (
     <Navbar expand="lg" sticky="top" className="navbar-custom" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="#/">
+        <Navbar.Brand href="/">
           <img  
             src={topbarlogo}
             width="100px"
@@ -27,39 +32,27 @@ function Topbar() {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
-          <Nav>
-            <Nav.Link
-              href="#/"
-              className={`nav-link ${location.pathname === '#/' ? 'active-link' : ''}`}
-            >
+        <Nav>
+          <Nav.Link as={Link} to="/" className={`nav-link ${activeLink === '/' ? 'active-link' : ''}`}>
               ABOUT
-            </Nav.Link>
-            <Nav.Link
-              href="#/timeline"
-              className={`nav-link ${location.pathname === '#/timeline' ? 'active-link' : ''}`}
-            >
+          </Nav.Link>
+          <Nav.Link as={Link} to="/timeline" className={`nav-link ${activeLink === '/timeline' ? 'active-link' : ''}`}>
               TIMELINE
-            </Nav.Link>
-            <Nav.Link
-              href="#/hackathon"
-              className={`nav-link ${location.pathname === '#/hackathon' ? 'active-link' : ''}`}
-            >
+          </Nav.Link>
+          <Nav.Link as={Link} to="/hackathon" className={`nav-link ${activeLink === '/hackathon' ? 'active-link' : ''}`}>
               HACKATHON
-            </Nav.Link>
-            <Nav.Link
-              href="#/faq"
-              className={`nav-link ${location.pathname === '#/faq' ? 'active-link' : ''}`}
-            >
+          </Nav.Link>
+          <Nav.Link as={Link} to="/faq" className={`nav-link ${activeLink === '/faq' ? 'active-link' : ''}`}>
               FAQ
-            </Nav.Link>
-            <Button
-              style={{ marginLeft: isMargin ? '20px' : '0px' }}
-              onClick={() => {
-                window.open("https://solux.tistory.com/")
-              }}
-            >
-              지원하기
-            </Button>
+          </Nav.Link>
+          <Button
+            style={{ marginLeft: isMargin ? '20px' : '0px' }}
+            onClick={() => {
+              window.open("https://solux.tistory.com/");
+            }}
+          >
+            지원하기
+          </Button>
           </Nav>
         </Navbar.Collapse>
       </Container>

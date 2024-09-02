a0d605e5bd9ff2fa28aed6932718f4e3ffa9259c
김세희 <lolry0207@sookmyung.ac.kr>
2024-09-03 00:35:21 +0900

    mod: timeline 페이지 변경 사항 수정

--- src/pages/TimelinePage.jsx
+++ src/pages/TimelinePage.jsx
@@ -6,32 +6,23 @@ import none from "../images/none.png";
 import { useMediaQuery } from 'react-responsive';
 
 const TimelineContainer = styled.div`
-    width: 100%;
     background-color: transparent;
 `
 
 const InfoContainer = styled.div`
     height: 90vh;
     width: 100%;
-    margin-top: 1vh;
-    margin-bottom: 6vh;
+    margin-top: 0;
+    margin-bottom: 40px;
     background-image: url(${background});
-    background-size: cover;
-    background-repeat: repeat;
-    background-position: top;
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: bottom;
     position: relative;
     display: flex;
     flex-direction: column;
-    justify-content: center;
     align-items: center;
-    text-align: left;
-`
-
-const TitleContainer = styled.div`
-    display: flex;
-    align-items: baseline;
-    background-color: transparent;
-    width: clamp(60vw, 1.5vw, 70vw);
+    justify-content: center;
 `
 
 const TitleText = styled.div`
@@ -45,49 +36,48 @@ const GrayTitleText = styled.div`
     font-size: clamp(0.6rem, 1vw, 0.8rem);
     color: #949494;
     background-color: transparent;
-    margin-left: 5px;
 `
 
 const GraySubtitleText = styled.div`
     font-weight: 400;
-    font-size: clamp(0.7rem, 1vw, 0.8rem);
+    width: 100%;
+    font-size: clamp(0.6rem, 100vw, 0.8rem);
     color: #949494;
     background-color: transparent;
-    width: clamp(60vw, 1.5vw, 70vw);
     margin-top: 20px;
+    text-align: center;
     line-height: 140%;
-    letter-spacing: 1px;
 `
 
 const SubtitleText = styled.div`
-    font-size: clamp(0.8rem, 1vw, 0.9rem);
+    font-size: clamp(0.6rem, 2vw, 0.8rem); 
     font-weight: 200;
     color: #B2B2B2;
     background-color: transparent;
-    width: clamp(60vw, 1.5vw, 70vw);
 `
 
 const PartContainer = styled.div`
+    width: 80%;
     background-color: transparent;
-    margin: 40px 0px 0px 0px;
-    width: clamp(60vw, 1.5vw, 70vw);
+    margin-top: 40px;
     display: flex;
     flex-direction: column;
-    align-items: flex-start;
+    align-items: center
 `
 
 const Part = styled.div`
     background-color: transparent;
     display: flex;
-    width: 80%;
+    flex-direction: column;
+    align-items: center;
 `
 
-const BlueText = styled.div`
-    color: #0057FF;
-    width: 7.5rem;
-    font-size: 1rem;
-    font-weight: 400;
+const TextButtonContainer = styled.div`
     background-color: transparent;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    width: 100%;
 `
 
 const TextButton = styled.div`
@@ -100,35 +90,60 @@ const TextButton = styled.div`
     padding-right: 0.5rem;
 `
 
-const WhiteText = styled.div`
-    width: clamp(60vw, 1.5vw, 70vw);
-    font-size: 1rem;
-    font-weight: 400;
-    background-color: transparent;
-`
-
 const ContentText = styled.div`
-    width: clamp(60vw, 1.5vw, 70vw);
-    margin-top: 30px;
-    font-size: clamp(0.9rem, 1vw, 1rem);
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center; 
     font-weight: 400;
     background-color: transparent;
-    line-height: 130%;
-    letter-spacing: 0.3px;
+    padding-bottom: 10px;
+    width: 90%;
+    max-width: 80%;
+    text-align: center; 
+    margin: 0 auto; 
+    box-sizing: border-box; 
+    overflow: hidden; 
+    align-items: center;
 `
 
-const TimetableIamge = styled.div`
-    width: clamp(30vw, 1.5vw, 35vw);
-    height: 250px;
-    background-image: url(${none});
-    background-size: cover;
-    background-repeat: no-repeat;
+const WhiteText = styled.span`
+    font-size: clamp(0.8rem, 2vw, 1rem); 
+    font-weight: 400;
+    background-color: transparent;
+`
+
+const BlueText = styled.span`
+    color: #0057FF;
+    font-size: clamp(0.8rem, 2vw, 1rem);
+    font-weight: 400;
+    background-color: transparent;
+`
+
+const GraySmallSubtitleText = styled.span`
+    font-size: clamp(0.6rem, 2vw, 0.7rem);
+    font-weight: 200;
+    color: #B2B2B2;
+    background-color: transparent;
+    margin-left: 5px;
 `
 
 const TimetableContainer = styled.div`
+    background-color: transparent;
     display: flex;
-    width: clamp(60vw, 1.5vw, 70vw);
-`
+    justify-content: center;
+    align-items: center;
+`;
+
+const TimetableImage = styled.div`
+    width: 40vw;
+    height: 50vh;
+    background-image: url(${none});
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    border-radius: 8px;
+`;
+
 
 const TimelinePage = () => {
     const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
@@ -145,16 +160,14 @@ const TimelinePage = () => {
     return (
         <TimelineContainer>
             <InfoContainer>
-                <TitleContainer>
-                    <TitleText>지원하기</TitleText>
-                    <GrayTitleText>APPLY</GrayTitleText>
-                </TitleContainer>
-                <SubtitleText>24. 09. 08 (SUN) 10:00 -24. 09. 14 (SAT) 23:59</SubtitleText>
+                <TitleText>지원하기</TitleText>
+                <GrayTitleText>APPLY</GrayTitleText>
+                <SubtitleText>24. 09. 08 (SUN) 10:00 - 24. 09. 14 (SAT) 23:59</SubtitleText>
 
                 { isMobile &&
                     <>
                         <PartContainer>
-                            <Part>
+                            <TextButtonContainer>
                                 <TextButton
                                     onClick={() => handleClick('DS')}
                                     style={{
@@ -175,10 +188,10 @@ const TimelinePage = () => {
                                 >
                                     개발
                                 </TextButton>
-                            </Part>
+                            </TextButtonContainer>
 
                             { isDS && <>
-                                <WhiteText style={{ marginTop: "10px"}}>DACOS 2기 부원 및 OB부원</WhiteText>
+                                <WhiteText style={{ marginTop: "10px", alignSelf: "flex-start"}}>DACOS 2기 부원 및 OB부원</WhiteText>
                                 <GraySubtitleText style={{ marginBottom: "50px"}}>
                                     * 5,000원의 참가비가 발생하니, 지원 시 참고해주시기 바랍니다.<br />
                                     * 교차 지원 불가능하며, 외부 인원 지원에 대해선 추후 공지 예정입니다.<br />
@@ -188,7 +201,7 @@ const TimelinePage = () => {
                                     onClick={() => {
                                         window.open("https://forms.gle/6KaV4nXxNoFtnS6p7") //다코스 모집 폼 링크
                                     }}
-                                    style = {{ height: "40px", width: "100px"}}
+                                    style = {{ height: "40px", width: "100px", alignSelf: "center"}}
                                     >
                                     지원하기
                                 </Button>
@@ -196,7 +209,7 @@ const TimelinePage = () => {
                             }
 
                             { !isDS && <>
-                                <WhiteText style={{ marginTop: "10px"}}>SOLUX 29기 부원 및 OB 부원 중 웹앱 개발 가능자</WhiteText>
+                                <WhiteText style={{ marginTop: "10px", alignSelf: "flex-start"}}>SOLUX 29기 부원 및 OB 부원 중 웹앱 개발 가능자</WhiteText>
                                 <GraySubtitleText style={{ marginBottom: "50px"}}>
                                     * 5,000원의 참가비가 발생하니, 지원 시 참고해주시기 바랍니다.<br />
                                     * 교차 지원 불가능하며, 외부 인원 지원에 대해선 추후 공지 예정입니다.<br />
@@ -220,12 +233,11 @@ const TimelinePage = () => {
                 { !isMobile && 
                     <>
                     <PartContainer>
-                        <Part>
+                        <Part style={{ marginTop: "10px"}}>
                             <BlueText>데이터 분석</BlueText>   
                             <WhiteText>DACOS 2기 부원 및 OB부원</WhiteText>
                         </Part>
-                        
-                        <Part>
+                        <Part style={{ marginTop: "10px"}}>
                             <BlueText>개발</BlueText>
                             <WhiteText>SOLUX 29기 부원 및 OB 부원 중 웹앱 개발 가능자</WhiteText>
                         </Part>
@@ -250,49 +262,50 @@ const TimelinePage = () => {
             </InfoContainer>
             
             <InfoContainer>
-                <TitleContainer>
-                    <TitleText>팀 공개</TitleText>
-                    <GrayTitleText>TEAM REVEAL</GrayTitleText>
-                </TitleContainer>
+                <TitleText>팀 공개</TitleText>
+                <GrayTitleText>TEAM REVEAL</GrayTitleText>
                 
                 <SubtitleText>2024. 10. 31 (THU)</SubtitleText>
                 
-                <ContentText>데이터 분석 2명, 프론트엔드 2명, 백엔드 2명으로 구성된 팀이 공개됩니다. (세부 인원수 변동 가능) <br />
-                구글폼에 기재해주신 연락처를 통해 오픈채팅방 개설 예정입니다.</ContentText>
-
-                <GraySubtitleText>*팀은 임의로 변경 불가능합니다</GraySubtitleText>
-
+                <ContentText style={{ marginTop: "60px"}}>
+                    <WhiteText>• 참여자 명단 및 오픈채팅방은</WhiteText> <BlueText>9월 20일 공개</BlueText> <WhiteText>됩니다.</WhiteText>
+                </ContentText>
+                <ContentText>
+                    <WhiteText>• 데이터 분석 2명, 프론트엔드 2명, 백엔드 2명으로 구성된 팀이 공개됩니다.</WhiteText> <GraySmallSubtitleText>(세부 인원수 변동 가능)</GraySmallSubtitleText>
+                </ContentText>
+                <ContentText>
+                    <WhiteText>• 구글폼에 기재해주신 연락처를 통해</WhiteText> <BlueText>오픈채팅방 개설 예정</BlueText> <WhiteText>입니다.</WhiteText>
+                </ContentText>
+                <GraySmallSubtitleText>*팀은 임의로 변경 불가능합니다</GraySmallSubtitleText>
             </InfoContainer>
 
             <InfoContainer>
-                <TitleContainer>
-                    <TitleText>사전준비</TitleText>
-                    <GrayTitleText>PRE - PREPARATION</GrayTitleText>
-                </TitleContainer>
+                <TitleText>사전준비</TitleText>
+                <GrayTitleText>PRE - PREPARATION</GrayTitleText>
                 
                 <SubtitleText>2024. 11. 01 (FRI) - 24. 11. 07 (THU)</SubtitleText>
                 
-                <ContentText>일주일간 사전 준비 기간을 거쳐 주제 선정을 비롯한 기획 및 데이터 준비가 필요합니다. 백-모델링 연결을 위한 사전 준비 또한 필요합니다. (관련 가이드 제공)
-                    <br />강의실을 대여해 회의 공간으로 제공할 예정이며, 강의실은 대여 후 공지 예정입니다.</ContentText>
+                <ContentText style={{ marginTop: "60px"}}>
+                    <WhiteText>• 일주일간 사전 준비 기간을 거쳐 주제 선정을 비롯한 </WhiteText> <BlueText>기획 및 데이터 준비가 필요</BlueText> <WhiteText>합니다.</WhiteText>
+                </ContentText>
+                <ContentText>
+                    <BlueText>• 백-모델링 연결</BlueText> <WhiteText>을 위한 사전 준비 또한 필요합니다.</WhiteText> <GraySmallSubtitleText>(관련 가이드 제공)</GraySmallSubtitleText>
+                </ContentText>
+                <ContentText>
+                    <WhiteText>• 강의실을 대여해 회의 공간으로 제공할 예정이며, 강의실은 대여 후 공지 예정입니다.</WhiteText>
+                </ContentText>
 
             </InfoContainer>
 
-            <InfoContainer>
-                <TitleContainer>
-                    <TitleText>연합 해커톤 진행</TitleText>
-                    <GrayTitleText>HACKATHON</GrayTitleText>
-                </TitleContainer>
+            <InfoContainer style={{ marginBottom: "20px"}}>
+                <TitleText>연합 해커톤 진행</TitleText>
+                <GrayTitleText>HACKATHON</GrayTitleText>
                 
-                <SubtitleText>2024. 11. 08 (FRI) 10:00 - 24. 11. 09 (SAT) 14:00</SubtitleText>
+                <div style={{ display: "flex", alignItems: "center"}}><SubtitleText>2024. 11. 08 (FRI) 10:00 - 24. 11. 09 (SAT) 14:00</SubtitleText> <GraySmallSubtitleText>* 무박 2일 진행</GraySmallSubtitleText></div>
                 
-                <ContentText>
-                    무박 2일 진행 <br />
-                    상세 장소는 추후 공지 예정이나 교내에서 진행됩니다.<br />
-                </ContentText>
                 <ContentText style={{ marginTop: "10px"}}>Time table</ContentText>
                 <TimetableContainer>
-                    <TimetableIamge />
-                    <TimetableIamge />
+                    <TimetableImage />
                 </TimetableContainer>
 
                 <GraySubtitleText style={{ marginTop: "5px"}}>*점심, 저녁, 다음날 아침 및 간단한 간식이 제공됩니다</GraySubtitleText>
